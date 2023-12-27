4ed6c9913975ee5868ad2dfaa1270f2d99d91257
Suly Pelayo <suly.pelayo@NOMS.MUGU>
2023-12-27 11:55:54 -0800

    database added for glider

--- Task-3/src/main/java/com/example/Task3/Glider.java
+++ Task-3/src/main/java/com/example/Task3/Glider.java
@@ -1,13 +1,21 @@
 package com.example.Task3;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.TableGenerator;
+
+@Entity
 public class Glider extends Aircraft {
+    @TableGenerator(name = "Table_Gen", table = "GliderTable", pkColumnName = "GliderIdName", valueColumnName = "GliderIdValue", pkColumnValue = "IdGen", initialValue = 1, allocationSize = 100 )
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "Table_Gen")
 
     private int numWings;
-
     public Glider(String name, int numWings, int id) {
         super(name, id);
         this.numWings = numWings;
-
     }
 
     public int getNumWings() {
@@ -23,4 +31,5 @@ public class Glider extends Aircraft {
         return "Glider Name: " + name + ", Number of Wings: " + numWings + "ID: " + id;
     }
 }
+
 
